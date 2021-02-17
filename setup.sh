107648a902ea8c0099618f18e729cf6b907e4240
root <root@mail.systar.my.id>
2021-02-17 18:52:34 +0700

    root : Wed Feb 17 18:52:34 WIB 2021

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt install postfix courier-imap courier-pop squirrelmail opendkim opendkim-tools mailutils certbot letsencrypt -y
+read -p 'Enter SubDomain [mail.example.com] : ' SUBDOMAIN
+read -p 'Enter RootDomain [example.com] : ' DOMAIN
+apt-get -o "DPkg::Options::=--force-confdef" install postfix courier-imap courier-pop squirrelmail opendkim opendkim-tools mailutils certbot letsencrypt -y
+certbot certonly -d $DOMAIN --standalone
+
 
